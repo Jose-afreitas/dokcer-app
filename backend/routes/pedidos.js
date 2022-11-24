e23cb9e7232434f24b90bc60043e0731a534415d
Jose-afreitas <josealmeida106@gmail.com>
2022-11-24 14:26:23 -0300

    wip - backend-produtos,pedidos

--- backend/routes/pedidos.js
+++ backend/routes/pedidos.js
@@ -1,38 +1,219 @@
+const { request, respose } = require('express');
 const express = require('express');
-
 const router = express.Router();
+const mysql = require('../config/mysql').pool;
 
 
-router.get('/', (req, res, next) => {
-    res.status(200).send({
-        message: 'Retorna os Pedidos'
-    });
-});
+//Inserindo um registro
 
 router.post('/', (req, res, next) => {
-
-    const pedido = {
-        id_produto: req.body.id_produto,
-        quandtidade: req.body.quantidade
-    }
-    res.status(201).send({
-        message: 'Inserindo um Pedidos',
-        pedidoCriado: pedido
-    });
+  //validação
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    conn.query(
+      'SELECT * FROM produtos WHERE id_produto = ?;',
+      [req.body.id_produto],
+      (error, result, field) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        if (result.length == 0) {
+          return res.status(404).send({
+            message: 'Não possui produto registrado com esse ID'
+          })
+        }
+        //inserindo um registro depois da verificação
+        conn.query(
+          'INSERT INTO ecommerce.pedidos(id_produto, quantidade) VALUES (?,?)',
+          [req.body.id_produto, req.body.quantidade],
+          (error, result, field) => {
+            conn.release();
+            if (error) { return res.status(500).send({ error: error }) }
+            const response = {
+              message: 'Registro inserido com sucesso',
+              pedidoCriado: {
+                id_pedido: result.id_pedido,
+                id_produto: req.body.id_produto,
+                quantidade: req.body.quantidade,
+                request: {
+                  tipo: 'POST',
+                  descricao: 'Inserido um registro',
+                  url: process.env.URL_POST_PEDIDOS
+                }
+              }
+            }
+            return res.status(200).send(response)
+          }
+        )
+      }
+    )
+  });
 });
 
+
+
+
+
+// //retorna todos os registros
+router.get('/', (req, res, next) => {
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    conn.query(
+      // 'SELECT * FROM  ecommerce.pedidos;'
+      `SELECT ped.id_pedido, ped.quantidade, prod.nome, prod.preco
+         FROM ecommerce.produtos as prod 
+         JOIN ecommerce.pedidos as ped 
+           ON prod.id_produto = ped.id_produto;`,
+
+      (error, result, fields) => {
+        conn.release();
+        if (error) { return res.status(500).send({ error: error }) }
+        const response = {
+          // quantidade: result.length,
+          pedido: result.map(pedido => {
+            return {
+              id_pedido: pedido.id_pedido,
+              quantidade: pedido.quantidade,
+              produto: {
+                id_produto: pedido.id_produto,
+                nome: pedido.nome,
+                preco: pedido.preco
+              },
+              request: {
+                tipo: 'GET',
+                descricao: 'Retorna os detalhes de um registro específico',
+                url: process.env.URL_GET_PEDIDOS + pedido.id_pedido
+              }
+            }
+          })
+        }
+        return res.status(200).send(response);
+      }
+    )
+  });
+});
+
+
+
+
+// Retorna apenas um registro
 router.get('/:id_pedido', (req, res, next) => {
-    const id = req.params.id_pedido
-    if (id === 'especial') {
-        res.status(200).send({
-            message: 'Você descobriu o ID especial',
-            id: id
-        })
-    } else {
-        res.status(200).send({
-            message: "Você passou um ID"
-        });
-    }
-});
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    conn.query(
+      // 'SELECT * FROM ecommerce.pedidos WHERE id_pedido = ?;',
+
+      `SELECT ped.quantidade , prod.nome, prod.preco, ped.id_pedido, prod.id_produto
+          FROM ecommerce.produtos as prod 
+          JOIN ecommerce.pedidos as ped 
+            ON prod.id_produto = ped.id_produto
+         WHERE ped.id_pedido =?;`,
+
+      [req.params.id_pedido, req.params.id_produto],
+      (error, result, field) => {
+        conn.release();
+        if (error) { return res.status(500).send({ error: error }) }
+        if (result.length == 0) {
+          return res.status(404).send({
+            message: 'Não foi encontrado o registro com o ID informado'
+          })
+        }
+        const response = {
+          pedido: {
+            id_pedido: result[0].id_pedido,
+            quantidade: result[0].quantidade,
+            produto: {
+              id_produto: result[0].id_produto,
+              nome: result[0].nome,
+              preco: result[0].preco
+            },
+            request: {
+              tipo: 'POST',
+              descricao: 'Retorna todos os registros',
+              url: process.env.URL_GET_PEDIDOS
+            }
+          }
+        }
+        return res.status(200).send(response)
+      }
+    )
+  })
+})
+
+
+
+
+//Editando registros
+router.patch('/', (req, res, next) => {
+  mysql.getConnection((error, conn) => {
+    if (error) { res.status(500).send({ error: error }) }
+    conn.query(
+
+      `UPDATE pedidos SET id_produto = ? , quantidade = ? WHERE id_pedido = ?`,
+      [req.body.id_produto, req.body.quantidade, req.body.id_pedido,],
+      (error, result, field) => {
+        conn.release();
+        if (error) { return res.status(500).send({ error: error }) }
+        const response = {
+          message: 'Registro Atualizado com Sucesso',
+          PedidoAtualizado: {
+            id_pedido: req.body.id_pedido,
+            id_produto: req.body.id_produto,
+            quantidade: req.body.quantidade,
+            request: {
+              tipo: 'GET',
+              descricao: 'Retorna os detalhes de um registro específico',
+              url: process.env.URL_GET_PEDIDOS + req.body.id_pedido
+            }
+          }
+        }
+
+        return res.status(202).send(response)
+      }
+    )
+  })
+})
+
+
+
+
+
+//Excluindo registros
+router.delete('/', (req, res, next) => {
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    conn.query(
+      `DELETE FROM pedidos WHERE id_pedido = ?;`,
+      [req.body.id_pedido],
+      (error, result, field) => {
+        conn.release();
+        if (error) { return res.status(500).send({ error: error }) }
+        const response = {
+          message: 'Registro Excluido com sucesso',
+          request: {
+            tipo: 'POST',
+            descricao: 'Insere um registro',
+            url: process.env.URL_POST_PEDIDOS,
+            body: {
+              id_produto: 'number',
+              quantidade: 'Number'
+            }
+          }
+        }
+        return res.status(202).send(response)
+      }
+    )
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
--- backend/routes/produtos.js
+++ backend/routes/produtos.js
@@ -1,50 +1,166 @@
+const { request, response } = require('express');
 const express = require('express');
 const router = express.Router();
-const mysql = require('../mysql').pool;
+const mysql = require('../config/mysql').pool;
+require('dotenv').config();
 
 
+
+// Inserindo um registro
 router.post('/', (req, res, next) => {
-    mysql.getConnection((error, conn) => {
-        conn.query(
-            ' INSERT IGNORE INTO produtos (nome, preco) VALUES (?,?)',
-            [req.body.nome, req.body.preco],
-            (error, result, field) => {
-                conn.release();
-                if (error) {
-                    return res.status(500).send({
-                        error: error,
-                        res: null
-                    });
-                }
-                res.status(201).send({
-                    message: 'Produto inserido com sucesso',
-                    id_produto: result.insertId
-                });
-            }
-        );
-
-    });
-});
-
-
-router.get('/', (req, res, next) => {
-    mysql.getConnection((error, conn) => {
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    conn.query(
+      ' INSERT IGNORE INTO IF NOT exists produtos (nome, preco) VALUES (?,?);',
+      [req.body.nome, req.body.preco],
+      (error, result, field) => {
+        conn.release();
         if (error) { return res.status(500).send({ error: error }) }
-        conn.query
-            ('SELECT * FROM ecommerce.produtos;',
-                (error, result, fields) => {
-
-                    if (error) { return res.status(500).send({ error: error }) }
-                    return res.status(200).send({ response: result })
-
-
-                });
-    });
+        const response = {
+          mensage: 'produto inserido com sucesso',
+          produtoCriado: {
+            id_produto: result.id_produto,
+            nome: req.body.nome,
+            preco: req.body.preco,
+            request: {
+              tipo: 'POST',
+              descricao: 'Inserindo um registro',
+              url: process.env.URL_POST_PRODUTOS
+            }
+          }
+        }
+        return res.status(201).send(response);
+      }
+    );
+  });
 });
 
 
+//Retornando todos os registros
+router.get('/', (req, res, next) => {
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    conn.query
+      ('SELECT * FROM ecommerce.produtos;',
+        (error, result, fields) => {
+          // conn.release();
+          if (error) { return res.status(500).send({ error: error }) }
+          const response = {
+            quantidade: result.length,
+            produtos: result.map(prod => {
+              return {
+                id_produto: prod.id_produto,
+                nome: prod.nome,
+                preco: prod.preco,
+                Request: {
+                  tipo: 'GET',
+                  descricao: 'Retorna os detalhes de apenas um registro',
+                  url: process.env.URL_GET_PRODUTOS + prod.id_produto
+                }
 
-router.patch
+              }
+            })
+          }
+          return res.status(200).send(response)
+        });
+  });
+});
+
+
+//retorna apenas um registro
+router.get('/:id_produto', (req, res, next) => {
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    conn.query(
+      'SELECT * FROM ecommerce.produtos WHERE id_produto = ?;',
+      [req.params.id_produto],
+      (error, result, field) => {
+        conn.release();
+        if (error) { return res.status(500).send({ error: error }) }
+        if (result.length == 0) {
+          return res.status(404).send({
+            message: 'Não foi encontrado registro com o ID informado'
+          })
+        }
+        const response = {
+          produto: {
+            id_produto: result[0].id_produto,
+            nome: result[0].nome,
+            preco: result[0].preco,
+            request: {
+              tipo: 'POST',
+              descricao: 'retorna todos os registros',
+              url: process.env.URL_GET_PRODUTOS
+            }
+          }
+        }
+        return res.status(200).send(response);
+      }
+    )
+  })
+})
+
+
+//Editando registros
+router.patch('/', (req, res, next) => {
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    conn.query(
+      `UPDATE produtos SET nome = ?,preco = ? WHERE id_produto = ?`,
+      [req.body.nome, req.body.preco, req.body.id_produto],
+      (error, result, field) => {
+        conn.release();
+        if (error) { return res.status(500).send({ error: error }) }
+        const response = {
+          mensage: 'produto Atualizado com sucesso',
+          produtoAtualizado: {
+            id_produto: req.body.id_produto,
+            nome: req.body.nome,
+            preco: req.body.preco,
+            request: {
+              tipo: 'GET',
+              descricao: 'Retorna os detalhes de um produto específico',
+              url: process.env.URL_GET_PRODUTOS + req.body.id_produto
+            }
+          }
+        }
+        return res.status(202).send(response);
+      }
+    );
+
+  });
+});
+
+
+// Excluindo registros
+router.delete('/', (req, res, next) => {
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    conn.query(
+      `DELETE FROM produtos WHERE id_produto = ?`,
+      [req.body.id_produto],
+      (error, result, field) => {
+        conn.release();
+        if (error) { return res.status(500).send({ error: error }) }
+        const response = {
+          message: 'Registro removido com sucesso',
+          request: {
+            tipo: 'POST',
+            descrocao: 'insere um registro',
+            url: process.env.URL_POST_PRODUTOS,
+            body: {
+              nome: 'String',
+              preco: 'Number'
+
+            }
+          }
+        }
+        return res.status(202).send(response);
+      }
+    );
+  });
+});
+
 
 
 module.exports = router;
